6aa9af90b0d245b07d1c9e1af94cb1c89992e07d
ArthurGalanti <arthur.galanti@hotmail.com>
2023-04-30 16:14:59 -0300

    Adicionado Value Objects e Enumerador

--- PaymentContext.Domain/Entities/PaypalPayment.cs
+++ PaymentContext.Domain/Entities/PaypalPayment.cs
@@ -1,8 +1,10 @@
-﻿namespace PaymentContext.Domain.Entities;
+﻿using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Domain.Entities;
 
 public class PaypalPayment  : Payment
 {
-    public PaypalPayment(DateTime paidDate, DateTime expireDate, decimal total, decimal totalPaid, string payer, string document, string address, string email, string transactionCode) : base(paidDate, expireDate, total, totalPaid, payer, document, address, email)
+    public PaypalPayment(DateTime paidDate, DateTime expireDate, decimal total, decimal totalPaid, string payer, Document document, string address, Email email, string transactionCode) : base(paidDate, expireDate, total, totalPaid, payer, document, address, email)
     {
         TransactionCode = transactionCode;
     }
--- PaymentContext.Tests/Entities/StudentTests.cs
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -1,4 +1,6 @@
 ﻿using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObjects;
 
 namespace PaymentContext.Tests;
 
@@ -8,7 +10,7 @@ public class StudentTests
     [TestMethod]
     public void TestMethod1()
     {
-        var student = new Student("Arthur", "Galanti", "123456789", "arthur.galanti@hotmail.com");
+        var student = new Student(new Name("Arthur", "Galanti"), new Document("123456789", EDocumentType.CPF), new Email("arthur.galanti@hotmail.com"));
         
     }
 }
